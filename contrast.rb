e6aabfa37d4bab6f0d441219c032d59df632d938
Beorn Facchini <beornf@gmail.com>
2015-02-24 09:58:40 +1100

    Tron high contrast theme.

--- contrast.rb
+++ contrast.rb
@@ -1,8 +1,19 @@
 require 'colorable'
 include Colorable
 
-hex = '#14191f'
-c = Color.new(hex).hsb
-c[1] -= 10
-out = Color.new(HSB.new(*c)).hex.downcase
-puts out.inspect
+out = File.open('schemes/tron_dark.yml', 'w')
+
+File.open('schemes/tron.yml', 'r') do |input|
+  input.each_line do |line|
+    if line.include? 'base0'
+      comp = line.split('"')
+      hex = "##{comp[1]}"
+      c = Color.new(hex).hsb
+      c[1] -= 6 if c[1] >= 6
+      comp[1] = Color.new(HSB.new(*c)).hex.sub('#', '').downcase
+      out.write(comp.join('"'))
+    else
+      out.write(line)
+    end
+  end
+end
