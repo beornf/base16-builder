6d74b4b08d2bb38582318ba741ba29b839ddf038
Beorn Facchini <beornf@gmail.com>
2015-02-24 20:01:20 +1100

    Tron final theme.

--- contrast.rb
+++ contrast.rb
@@ -1,15 +1,15 @@
 require 'colorable'
 include Colorable
 
-out = File.open('schemes/tron_dark.yml', 'w')
+out = File.open('schemes/tron.yml', 'w')
 
-File.open('schemes/tron.yml', 'r') do |input|
+File.open('schemes/tron-original.yml', 'r') do |input|
   input.each_line do |line|
     if line.include? 'base0'
       comp = line.split('"')
       hex = "##{comp[1]}"
       c = Color.new(hex).hsb
-      c[1] -= 6 if c[1] >= 6
+      c[1] -= 4 if c[1] >= 4
       comp[1] = Color.new(HSB.new(*c)).hex.sub('#', '').downcase
       out.write(comp.join('"'))
     else
